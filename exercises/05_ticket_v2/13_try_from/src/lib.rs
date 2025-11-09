60882a79bc5d648406611c7e9f3afc506419405c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-11-09 17:56:48 +0000

    TryFrom<T> turn arbitrary input into a domain‑specific enum (Status) while gracefully handling errors.

---
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -0,0 +1,70 @@
+// TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
+//  The parsing should be case-insensitive.
+
+use std::convert::TryFrom;
+
+#[derive(Debug, PartialEq)]
+pub struct ParseStatusError;
+
+#[derive(Debug, PartialEq, Clone)]
+enum Status {
+    ToDo,
+    InProgress,
+    Done,
+}
+
+impl TryFrom<String> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        // Reuse the &str implementation.
+        // Self::try_from(&value)
+        Self::try_from(value.as_str())
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.eq_ignore_ascii_case("todo") {
+            Ok(Status::ToDo)
+        } else if value.eq_ignore_ascii_case("inprogress") {
+            Ok(Status::InProgress)
+        } else if value.eq_ignore_ascii_case("done") {
+            Ok(Status::Done)
+        } else {
+            Err(ParseStatusError)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn test_try_from_string() {
+        let status = Status::try_from("ToDO".to_string()).unwrap();
+        assert_eq!(status, Status::ToDo);
+
+        let status = Status::try_from("inproGress".to_string()).unwrap();
+        assert_eq!(status, Status::InProgress);
+
+        let status = Status::try_from("Done".to_string()).unwrap();
+        assert_eq!(status, Status::Done);
+    }
+
+    #[test]
+    fn test_try_from_str() {
+        let status = Status::try_from("todo").unwrap();
+        assert_eq!(status, Status::ToDo);
+
+        let status = Status::try_from("inprogress").unwrap();
+        assert_eq!(status, Status::InProgress);
+
+        let status = Status::try_from("done").unwrap();
+        assert_eq!(status, Status::Done);
+    }
+}
