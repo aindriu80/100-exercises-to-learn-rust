8d01d7b73950c8dd367204b9c1d4e44a802eace6
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-11-15 15:41:46 +0000

    Threads - slices, summing with iterators, joining threads & error handling, static lifetimes

---
+++ exercises/07_threads/02_static/src/lib.rs
@@ -0,0 +1,61 @@
+// TODO: Given a static slice of integers, split the slice into two halves and
+//  sum each half in a separate thread.
+//  Do not allocate any additional memory!
+use std::thread;
+
+pub fn sum(slice: &'static [i32]) -> i32 {
+    let len = slice.len();
+
+    match len {
+        0 => 0,
+        1 => slice[0],
+        _ => {
+            // Midpoint - integer division gives us floor(len/2).
+            // For odd length, the right half will be one element longer.
+            let mid = len / 2;
+            let (left, right) = slice.split_at(mid);
+
+            // Spawn a thread for each half.  The closures capture "reference",
+            // not copies of the data.
+            let left_handle = std::thread::spawn(move || left.iter().sum::<i32>());
+            let right_handle = std::thread::spawn(move || right.iter().sum::<i32>());
+
+            left_handle.join().unwrap() + right_handle.join().unwrap()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        static ARRAY: [i32; 0] = [];
+        assert_eq!(sum(&ARRAY), 0);
+    }
+
+    #[test]
+    fn one() {
+        static ARRAY: [i32; 1] = [1];
+        assert_eq!(sum(&ARRAY), 1);
+    }
+
+    #[test]
+    fn five() {
+        static ARRAY: [i32; 5] = [1, 2, 3, 4, 5];
+        assert_eq!(sum(&ARRAY), 15);
+    }
+
+    #[test]
+    fn nine() {
+        static ARRAY: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        assert_eq!(sum(&ARRAY), 45);
+    }
+
+    #[test]
+    fn ten() {
+        static ARRAY: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        assert_eq!(sum(&ARRAY), 55);
+    }
+}
