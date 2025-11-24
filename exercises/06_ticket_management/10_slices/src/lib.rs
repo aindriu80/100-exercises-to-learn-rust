ca1a7c044a385389c0764a24d6673827cad61aac
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-11-24 19:55:32 +0000

    Implemented a 'sum' function that accepts a slice of 'u32' and returns the total of its elements.

---
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -0,0 +1,38 @@
+// TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
+//  elements in the slice.
+fn sum(slice: &[u32]) -> u32 {
+    let mut total = 0;
+    for &x in slice {
+        total += x;
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let v = vec![];
+        assert_eq!(sum(&v), 0);
+    }
+
+    #[test]
+    fn one_element() {
+        let v = vec![1];
+        assert_eq!(sum(&v), 1);
+    }
+
+    #[test]
+    fn multiple_elements() {
+        let v = vec![1, 2, 3, 4, 5];
+        assert_eq!(sum(&v), 15);
+    }
+
+    #[test]
+    fn array_slice() {
+        let v = [1, 2, 3, 4, 5];
+        assert_eq!(sum(&v), 15);
+    }
+}
